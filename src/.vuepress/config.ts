8454599f5c4156d4437835e8ac59fd94081f0ca1
Linc <762699299@qq.com>
2024-03-16 20:28:38 +0800

    add: my config

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,24 +1,35 @@
-import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { defineUserConfig } from "vuepress"; // 从 "vuepress" 导入定义用户配置的方法
+import theme from "./theme.js"; // 从当前目录下的 "theme.js" 文件导入主题
 
+// 使用定义的用户配置导出一个默认的配置对象
 export default defineUserConfig({
-  base: "/",
+  // ******************* 配置类 - 特殊 **********************
 
-  locales: {
+  theme, // 使用导入的主题
+
+  // ******************* 配置类 - 导航 **********************
+
+  base: "/", // 设置基本目录路径
+
+  locales: { // 设置多语言配置
+    // "/en/": {
+    //   lang: "en-US", // 设定语言为美式英语
+    //   title: "Docs Demo", // 设定标题
+    //   description: "A docs demo for vuepress-theme-hope", // 设定描述
+    // },
     "/": {
-      lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
-    },
-    "/zh/": {
       lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
+      title: "Linc 的小站",
+      description: "Linc 的个人小站",
     },
   },
+  markdown: {
+    headers: {
+      level: [1,2,3,4,5,6] // 不然的话toc会受限
+    }
+  },
 
-  theme,
-
-  // Enable it with pwa
-  // shouldPrefetch: false,
+  // ******************* 配置类 - 其他 **********************
+  // 使网页成为PWA
+  // shouldPrefetch: false, // 是否预获取
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,206 +1,93 @@
-import { hopeTheme } from "vuepress-theme-hope";
-import { enNavbar, zhNavbar } from "./navbar/index.js";
-import { enSidebar, zhSidebar } from "./sidebar/index.js";
+import { hopeTheme } from "vuepress-theme-hope"; // 从 "vuepress-theme-hope" 导入 hope 主题
 
+// 导出默认的主题配置
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  // ******************* 信息类 *****************************
 
-  author: {
-    name: "Mr.Hope",
-    url: "https://mister-hope.com",
+  hostname: "http://192.168.0.101:8080/", //设置网站主机名
+  author: { // 设置作者信息
+    name: "LincZero", // 作者姓名
+    url: "https://github.com/LincZero/", // 作者网站链接
   },
+  repo: "LincZero/LincZero-Vuepress", // 仓库地址
+  iconAssets: "fontawesome-with-brands", // 使用 FontAwesome 图标库
+  logo: "/logo.svg", // 设置 logo 路径
 
-  iconAssets: "fontawesome-with-brands",
+  // ******************* 配置类 - 导航 **********************
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
-
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
-
-  docsDir: "src",
-
+  docsDir: "src", // 存放文档的目录
   locales: {
+    // "/en/": { // 英文
+    //   // navbar: enNavbar, // 导航栏
+    //   sidebar: "structure", // 侧边栏
+    //   // footer: "Default footer", // 页脚文本
+    //   // displayFooter: true, // 显示页脚
+    //   metaLocales: {
+    //     editLink: "Edit this page on GitHub", // Github上编辑该页的文本
+    //   },
+    // },
     "/": {
-      // navbar
-      navbar: enNavbar,
-
-      // sidebar
-      sidebar: enSidebar,
-
-      footer: "Default footer",
-
-      displayFooter: true,
-
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
-    },
-
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
-      // navbar
-      navbar: zhNavbar,
-
-      // sidebar
-      sidebar: zhSidebar,
-
-      footer: "默认页脚",
-
-      displayFooter: true,
-
-      // page meta
+      // navbar: zhNavbar,
+      sidebar: "structure",
+      // footer: "默认页脚",
+      // displayFooter: true,
       metaLocales: {
         editLink: "在 GitHub 上编辑此页",
       },
     },
   },
+  toc: true,
+  headerDepth: 6,
 
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": ["1234"],
-      "/zh/demo/encrypt.html": ["1234"],
-    },
-  },
+  // ******************* 配置类 - 内容 **********************
 
-  plugins: {
-    // You should generate and use your own comment service
-    comment: {
-      provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
-      category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
-    },
+  plugins: { // 插件配置
+    // comment: {}, // 评论系统的配置
 
-    components: {
-      components: ["Badge", "VPCard"],
-    },
-
-    // All features are enabled for demo, only preserve features you need here
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      codetabs: true,
-      component: true,
-      demo: true,
-      figure: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      mark: true,
-      stylize: [
+    mdEnhance: { // 扩展 Markdown 的功能
+      align: true, // 对齐
+      attrs: true, // 属性
+      // chart: true,  // 图表 install chart.js before enabling it
+      codetabs: true, // 代码标签
+      demo: true, // 演示
+      // component: true, // insert component easily
+      // echarts: true, // Echarts 图表工具，install echarts before enabling it
+      figure: true, // Figure 图表工具
+      // flowchart: true, // 流程图，install flowchart.ts before enabling it
+      // gfm: true, // GitHub 风格的 Markdown，gfm requires mathjax-full to provide tex support
+      imgLazyload: true, // 懒加载图片
+      imgSize: true, // 图片大小
+      include: true, // 包括
+      // katex: true, // TeX数学公式渲染工具 (二选一，katex), install katex before enabling it
+      // mathjax: true, // TeX数学公式渲染工具 (二选一，mathjax), mathjax-full before enabling it
+      mark: true, // 标记
+      // mermaid: true, // Mermaid 图表工具, install mermaid before enabling it
+      playground: { // 在线代码演示设置
+        presets: ["ts", "vue"], // 预设
+      },
+      // revealJs: { // install reveal.js before enabling it
+      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+      // },
+      stylize: [ // 样式化工具
         {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
+          matcher: "Recommended", // 匹配器
+          replacer: ({ tag }) => { // 替换器
+            if (tag === "em") // 如果标签为 "em"
               return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
+                tag: "Badge", // 替换为 "Badge" 标签
+                attrs: { type: "tip" }, // 设置属性
+                content: "Recommended", // 设置内容
               };
           },
         },
       ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-
-      // install chart.js before enabling it
-      // chart: true,
-
-      // insert component easily
-
-      // install echarts before enabling it
-      // echarts: true,
-
-      // install flowchart.ts before enabling it
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // install katex before enabling it
-      // katex: true,
-
-      // install mathjax-full before enabling it
-      // mathjax: true,
-
-      // install mermaid before enabling it
-      // mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // install @vue/repl before enabling it
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
+      sub: true, // 子项
+      sup: true, // 上标
+      tabs: true, // 标签
+      vPre: true, // Vue 预处理
+      // vuePlayground: true, // Vue 种植场, install @vue/repl before enabling it
     },
-
-    // install @vuepress/plugin-pwa and uncomment these if you want a PWA
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
-  },
-});
+  }
+},
+{ custom: true }
+);
