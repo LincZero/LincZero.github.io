403de5d5964daf82ba9193582b2ad43b13eb6634
Linc <762699299@qq.com>
2024-09-12 17:43:32 +0800

    add: (plugin) obsidian callout (replace github alert)

--- src/.vuepress/config_extendsMarkdown.ts
+++ src/.vuepress/config_extendsMarkdown.ts
@@ -1,5 +1,7 @@
 import ab_mdit from "./plugin/ABConvertManager/ab_mdit"
+import callout_mdit from "markdown-it-obsidian-callouts" // https://github.com/ebullient/markdown-it-obsidian-callouts
 
 export default  (md: markdownit) => {
   md.use(ab_mdit)
+  md.use(callout_mdit)
 }
