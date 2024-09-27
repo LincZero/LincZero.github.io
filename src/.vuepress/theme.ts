4a4fbf7cbfd85cde06cc2e5eda066ff01e89a4f3
Linc <762699299@qq.com>
2024-09-28 00:36:35 +0800

    chore: tidy up code comments

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -40,7 +40,7 @@ export default hopeTheme(
 
     // 插件类
     plugins: {
-      docsearch: {
+      docsearch: { // 文档搜索插件
         appId: "247IUCFQ4Q",
         apiKey: "b8883b05027bfa9fc73f21286ed7395e", // 707332eeb6a1daf0e6ecfbb63e27afbc
         indexName: "linczeroio"
@@ -52,37 +52,38 @@ export default hopeTheme(
       mdEnhance: { // 扩展 Markdown 的功能
         // mdit - 容器语法
         // alert: true, // 被 obsidain callout 代替
-        align: true, // mdit容器 - 对齐
-        attrs: true, // mdit容器 - 属性
-        demo: true, // mdit容器 - 代码演示
-        tabs: true, // mdit容器 - 标签
+        align: true,    // mdit容器 - 对齐
+        attrs: true,    // mdit容器 - 属性
+        demo: true,     // mdit容器 - 代码演示 // 注意变更: https://plugin-md-enhance.vuejs.press/zh/migration.html#%E5%8F%98%E6%9B%B4
+        tabs: true,     // mdit容器 - 标签
+        codetabs: true, // mdit容器 - 代码块分组
 
         // mdit - 内联语法
-        imgLazyload: true, // 懒加载图片
-        // imgSize: true, // 图片大小, ![Alt](/example.bmp =x300)
-        // include: true, // 导入其他文件，`<!-- @include: filename -->`，@WARN 启用的话会和mdit插件那边的 `md.render()` 冲突
-        mark: true, // ==高亮==
-        sub: true, // _子项_
-        sup: true, // ^上标^
+        imgLazyload: true,  // 懒加载图片
+        // imgSize: true,   // 图片大小, ![Alt](/example.bmp =x300)
+        // include: true,   // 导入其他文件，`<!-- @include: filename -->`，@WARN 启用的话会和mdit插件那边的 `md.render()` 冲突
+        mark: true,         // ==高亮==
+        sub: true,          // _子项_
+        sup: true,          // ^上标^
 
         // mdit - 文生图类
-        katex: true, // TeX数学公式渲染工具 (二选一，katex), install katex before enabling it
-        // mathjax: true, // TeX数学公式渲染工具 (二选一，mathjax), mathjax-full before enabling it
-        mermaid: true, // Mermaid 图表工具, install mermaid before enabling it
-        markmap: true, // 思维导图，pnpm add -D markmap-lib markmap-toolbar markmap-view
-        // plantuml: true, // Plantuml 图表工具
-        // chart: true,  // 图表 install chart.js before enabling it
-        // echarts: true, // Echarts 图表工具，install echarts before enabling it
-        figure: true, // Figure 图表工具
+        katex: true,        // TeX数学公式渲染工具 (二选一，katex), install katex before enabling it
+        // mathjax: true,   // TeX数学公式渲染工具 (二选一，mathjax), mathjax-full before enabling it
+        mermaid: true,      // Mermaid 图表工具, install mermaid before enabling it
+        markmap: true,      // 思维导图，pnpm add -D markmap-lib markmap-toolbar markmap-view
+        // plantuml: true,  // Plantuml 图表工具
+        // chart: true,     // 图表 install chart.js before enabling it
+        // echarts: true,   // Echarts 图表工具，install echarts before enabling it
+        figure: true,       // Figure 图表工具
         // flowchart: true, // 流程图，install flowchart.ts before enabling it
 
         // 其他
-        codetabs: true, // 代码块分组
         // component: true, // insert component easily
-        // gfm: true, // GFM 警告，类似ob的callout
-        playground: { // 在线代码演示设置
+        // gfm: true,       // Github风格的md，是CommonMark的超集，GitHub Flavored Markdown，见 https://github.github.com/gfm/
+        playground: {       // 在线代码演示设置
           presets: ["ts", "vue"], // 预设
         },
+        // vuePlayground: true, // Vue 实验场, install @vue/repl before enabling it
         // revealJs: { // install reveal.js before enabling it
         //   plugins: ["highlight", "math", "search", "notes", "zoom"],
         // },
@@ -100,7 +101,6 @@ export default hopeTheme(
           },
         ],
         vPre: true, // Vue 预处理，Mustache 语法
-        // vuePlayground: true, // Vue 种植场, install @vue/repl before enabling it
       },
     }
   },
