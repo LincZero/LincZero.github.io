32b96b02abb88e445abe1712835ca264fda1db27
Linc <762699299@qq.com>
2024-10-18 10:56:11 +0800

    update(plugin): anyblock v3.1.6-beta3

--- src/.vuepress/config_extendsMarkdown.ts
+++ src/.vuepress/config_extendsMarkdown.ts
@@ -1,9 +1,12 @@
 import callout_mdit from "markdown-it-obsidian-callouts" // https://github.com/ebullient/markdown-it-obsidian-callouts
 
+// import ab_mdit from "./plugin/ABConvertManager/dist/index_mdit.js"     // 编译版tsc (成功)
+// import ab_mdit from "./plugin/ABConvertManager/dist/index_mdit"        // 编译版tsup (使用失败 Error [ERR_MODULE_NOT_FOUND]: Cannot find package 'markdown-it')
+// import ab_mdit from "./plugin/ABConvertManager/dist/mdit-any-block.js" // 编译版vite
 import ab_mdit from "./plugin/ABConvertManager/src/index_mdit"            // 源码版
-
 // import ab_mdit from "any-block-converter-markdown-it"                  // npm版
-// JsDom。仅用于提供document对象支持 (如果Ob环境中则不需要，用ob自带document对象的)
+
+/*// JsDom。仅用于提供document对象支持 (如果Ob环境中则不需要，用ob自带document对象的)
 import jsdom from "jsdom"
 const { JSDOM } = jsdom
 const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`, {
