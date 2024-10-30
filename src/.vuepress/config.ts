adafc019cb50087407402261a7f515d5367ad396
Linc <762699299@qq.com>
2024-10-30 19:51:53 +0800

    feat: support json extension
    Can work with `NodeFlow` plugin

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -23,12 +23,35 @@ export default defineUserConfig({
       level: [1, 2, 3, 4, 5, 6] // 不然的话toc会受限
     },
   },
+  pagePatterns: ["**/*.md", "**/*.json", "!**/*.snippet.md", "!.vuepress", "!node_modules"], // "**/*.pdf"
 
   // ------------------ 扩展类 ------------------
   theme,
   alias,
   extendsMarkdown,
   plugins,
+
+  // ------------------ 扩展类 - 钩子 ------------
+  async onInitialized(app) {
+    /**
+     * 例如这里我对.json后缀进行处理 (需要先设置pagePatterns允许解析json，否则找不到json文件)
+     * 甚至可以不修改，而用一个新页面替换掉
+     */
+    for (let i = 0; i < app.pages.length; i++) {
+      const page = app.pages[i];
+      if (!page.path.endsWith(".json")) continue
+      // console.log("旧页面信息----------------------------------------\n", page)
+      // console.log("新页面信息----------------------------------------\n", newPage)
+      {
+        page.path = page.path+"/"
+        // page.frontmatter = newPage.frontmatter    // 保证能够打开
+        page.frontmatter.layout = 'Layout'
+        page.content = "```nodeflow-comfyui\n" + page.content + "\n```"
+        if(page.sfcBlocks.template?.contentStripped) page.sfcBlocks.template.contentStripped = // HTML内容以这个为准
+          app.markdown.render(page.content)       // 重渲染了
+      }
+    }
+  },
 
   // ------------------ 其他 ---------------------
   // 使网页成为PWA
