66d4d024de66d98722dda20306f6d7effdb9f100
LincZero <762699299@qq.com>
2024-11-01 06:51:38 +0000

    Deploying to gh-pages from @ LincZero/LincZero.github.io@38229c571446ffa1f96bef10e79a2cbc3f426353 🚀

---
+++ assets/CppProperties.json.html-CUl2ZzHu.js
@@ -0,0 +1,22 @@
+import{_ as e,c as a,d as t,o as s,r as o}from"./app-DTo3dt_i.js";const i={};function m(r,p){const n=o("VueFlow");return s(),a("div",null,[t(n,{type:"comfyui",data:`{
+  "configurations": [
+    {
+      "inheritEnvironments": [
+        "msvc_x86"
+      ],
+      "name": "x86-Debug",
+      "includePath": [
+        "\${env.INCLUDE}",
+        "\${workspaceRoot}\\\\**"
+      ],
+      "defines": [
+        "WIN32",
+        "_DEBUG",
+        "UNICODE",
+        "_UNICODE"
+      ],
+      "intelliSenseMode": "windows-msvc-x86"
+    }
+  ]
+}
+`})])}const c=e(i,[["render",m],["__file","CppProperties.json.html.vue"]]),d=JSON.parse('{"path":"/MdNote_Public/01.%20DesignAndDevelop/Develop/01.%20Language/C__/10.%20%E7%9B%B8%E5%85%B3%E4%BB%A3%E7%A0%81/CppProperties.json","title":"","lang":"zh-CN","frontmatter":{"description":"{ \\"configurations\\": [ { \\"inheritEnvironments\\": [ \\"msvc_x86\\" ], \\"name\\": \\"x86-Debug\\", \\"includePath\\": [ \\"env.INCLUDE\\",\\"{workspaceRoot}\\\\**\\" ], \\"defines\\": [ \\"WIN32\\", \\"_DEBUG\\", \\"UNICO...","head":[["meta",{"property":"og:url","content":"http://192.168.0.101:8080/MdNote_Public/01.%20DesignAndDevelop/Develop/01.%20Language/C__/10.%20%E7%9B%B8%E5%85%B3%E4%BB%A3%E7%A0%81/CppProperties.json"}],["meta",{"property":"og:site_name","content":"Linc 的小站"}],["meta",{"property":"og:description","content":"{ \\"configurations\\": [ { \\"inheritEnvironments\\": [ \\"msvc_x86\\" ], \\"name\\": \\"x86-Debug\\", \\"includePath\\": [ \\"env.INCLUDE\\",\\"{workspaceRoot}\\\\**\\" ], \\"defines\\": [ \\"WIN32\\", \\"_DEBUG\\", \\"UNICO..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"article:author","content":"LincZero"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":null,\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"LincZero\\",\\"url\\":\\"https://github.com/LincZero/\\"}]}"]],"layout":"Layout"},"headers":[],"git":{"createdTime":null,"updatedTime":null,"contributors":[]},"readingTime":{"minutes":0.06,"words":18},"filePathRelative":"MdNote_Public/01. DesignAndDevelop/Develop/01. Language/C++/10. 相关代码/CppProperties.json","excerpt":"<p>{\\n\\"configurations\\": [\\n{\\n\\"inheritEnvironments\\": [\\n\\"msvc_x86\\"\\n],\\n\\"name\\": \\"x86-Debug\\",\\n\\"includePath\\": [\\n\\"<span v-pre=\\"\\" class=\\"katex\\"><span class=\\"katex-mathml\\"><math xmlns=\\"http://www.w3.org/1998/Math/MathML\\"><semantics><mrow><mrow><mi>e</mi><mi>n</mi><mi>v</mi><mi mathvariant=\\"normal\\">.</mi><mi>I</mi><mi>N</mi><mi>C</mi><mi>L</mi><mi>U</mi><mi>D</mi><mi>E</mi></mrow><mi mathvariant=\\"normal\\">\\"</mi><mo separator=\\"true\\">,</mo><mi mathvariant=\\"normal\\">\\"</mi></mrow><annotation encoding=\\"application/x-tex\\">{env.INCLUDE}\\",\\n        \\"</annotation></semantics></math></span><span class=\\"katex-html\\" aria-hidden=\\"true\\"><span class=\\"base\\"><span class=\\"strut\\" style=\\"height:0.8889em;vertical-align:-0.1944em;\\"></span><span class=\\"mord\\"><span class=\\"mord mathnormal\\">e</span><span class=\\"mord mathnormal\\">n</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.03588em;\\">v</span><span class=\\"mord\\">.</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.07847em;\\">I</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.07153em;\\">NC</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.10903em;\\">LU</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.02778em;\\">D</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.05764em;\\">E</span></span><span class=\\"mord\\">\\"</span><span class=\\"mpunct\\">,</span><span class=\\"mspace\\" style=\\"margin-right:0.1667em;\\"></span><span class=\\"mord\\">\\"</span></span></span></span>{workspaceRoot}\\\\**\\"\\n],\\n\\"defines\\": [\\n\\"WIN32\\",\\n\\"_DEBUG\\",\\n\\"UNICODE\\",\\n\\"_UNICODE\\"\\n],\\n\\"intelliSenseMode\\": \\"windows-msvc-x86\\"\\n}\\n]\\n}</p>","autoDesc":true}');export{c as comp,d as data};
--- assets/clone-BM7aTg_p.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./graph-BN1QK3MN.js";var a=4;function n(o){return r(o,a)}export{n as c};
