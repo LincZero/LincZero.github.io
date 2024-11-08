cf2e419010a1e41c26ad52c5b598459ce7199e34
Linc <762699299@qq.com>
2024-11-08 15:24:59 +0800

    chore: init raw branch

--- src/.vuepress/theme_navbar.ts
+++ src/.vuepress/theme_navbar.ts
@@ -2,35 +2,5 @@ import { navbar } from 'vuepress-theme-hope';
 
 export const navbarConfig = navbar([
   // raw:
-  // {text: "/", link: "/"},
-
-  {
-    text: "公有文档",
-    //link: "/MdNote_Public/",
-    prefix: "/MdNote_Public/",
-    children: [
-      { text: "引导", link: "Guide/"},
-      { text: "产品文档", link: "ProductDoc/"},
-      { text: "设计开发与数据生产", link: "01.%20DesignAndDevelop/"},
-      { text: "十二门类与纯粹知识", link: "02.%20SubjectAndKnowledge/"},
-      { text: "生活技能与生活生产", link: "03.%20SkillAndProduction/"},
-      { text: "全部", link: ""},
-    ]
-  },
-  {
-    text: "其他文档库",
-    link: "/MdNote_Other/",
-  },
-  {
-    text: "私有文档",
-    link: "/MdNote_Private/",
-  },
-  {
-    text: "特殊权限 (路径隐藏)",
-    link: "/MdNote_Hide1/",
-  },
-  {
-    text: "博客页 (路径隐藏)",
-    link: "/MdNote_Blog1/",
-  },
+  {text: "/", link: "/"},
 ]);
--- src/.vuepress/theme_sidebar.ts
+++ src/.vuepress/theme_sidebar.ts
@@ -3,17 +3,6 @@ import { sidebar } from 'vuepress-theme-hope';
 export const sidebarConfig = sidebar(
   {
     // raw:
-    // "/": "structure",
-
-    "/MdNote_Public/": "structure", // 如果这里重复了，其实会有该问题：当你点击子菜单时，侧边栏会发生改变，变为更小的级别
-      "/MdNote_Public/Guide": "structure",
-      "/MdNote_Public/ProductDoc": "structure",
-      "/MdNote_Public/01.%20DesignAndDevelop": "structure",
-      "/MdNote_Public/02.%20SubjectAndKnowledge": "structure",
-      "/MdNote_Public/03.%20SkillAndProduction": "structure",
-    "/MdNote_Other/": "structure",
-    "/MdNote_Work/": "structure",
-    "/MdNote_Private/": "structure",
-    // "/Public_Docs/": "structure", // 特殊路径，内容为public文件夹对应的虚拟页
+    "/": "structure",
   }
 );
