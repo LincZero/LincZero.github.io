126468906a92a34db12daf6d564b78705c09bcdd
Linc <762699299@qq.com>
2025-01-06 20:48:27 +0800

    update(anyblock): version v3.1.9 (patch2)

--- src/.vuepress/plugin/ABConvertManager/src/converter/abc_deco.ts
+++ src/.vuepress/plugin/ABConvertManager/src/converter/abc_deco.ts
@@ -67,27 +67,28 @@ const abc_fold = ABConvert.factory({
     mid_el.appendChild(sub_button)
     mid_el.appendChild(sub_el)
 
+    // 二选一。仅ob环境，mdit环境不支持
     // 特殊：如果折叠内容是列表格。将该处理器的折叠行为修改该按钮的折叠功能
-    if (sub_el.classList.contains("ab-list-table")) {
-      const btn = sub_el.querySelector(":scope>.ab-table-fold")
-      if (btn) {
-        // 1. 回溯原折叠
-        fn_fold()
-        sub_button.textContent = "全部折叠/展开"
-        // 2. 使用新折叠
-        const fn_fold2 = ()=>{
-          const clickEvent = new MouseEvent("click", {
-            view: window,
-            bubbles: true,
-            cancelable: true
-          });
-          btn.dispatchEvent(clickEvent);
-        }
-        fn_fold2()
-        // 3. 按钮功能替换
-        sub_button.onclick = fn_fold2
-      }
-    }
+    // if (sub_el.classList.contains("ab-list-table")) {
+    //   const btn = sub_el.querySelector(":scope>.ab-table-fold")
+    //   if (btn) {
+    //     // 1. 回溯原折叠
+    //     fn_fold()
+    //     sub_button.textContent = "全部折叠/展开"
+    //     // 2. 使用新折叠
+    //     const fn_fold2 = ()=>{
+    //       const clickEvent = new MouseEvent("click", {
+    //         view: window,
+    //         bubbles: true,
+    //         cancelable: true
+    //       });
+    //       btn.dispatchEvent(clickEvent);
+    //     }
+    //     fn_fold2()
+    //     // 3. 按钮功能替换
+    //     sub_button.onclick = fn_fold2
+    //   }
+    // }
     return content
   }
 })
