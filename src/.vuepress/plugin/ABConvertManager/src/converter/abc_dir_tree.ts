9d301fe4cddd0e9e79fc46f924c7c375372e2e81
Linc <762699299@qq.com>
2025-01-30 15:24:57 +0800

    update(anyblock): v3.1.10-beta2

--- src/.vuepress/plugin/ABConvertManager/src/converter/abc_dir_tree.ts
+++ src/.vuepress/plugin/ABConvertManager/src/converter/abc_dir_tree.ts
@@ -207,43 +207,43 @@ export class DirProcess{
         const tr = l_tr[i]
         // 1. 二选一，嵌入内联onclick
         // 当前mdit使用
-        //tr.setAttribute("onclick", `
-        //  const tr = this
-        //  const l_tr = tr.parentNode.querySelectorAll("tr")
-        //  const i = ${i}
-        //  const tr_level = Number(tr.getAttribute("tr_level"))
-        //  if (isNaN(tr_level)) return
-        //  const tr_isfold = tr.getAttribute("is_fold")
-        //  if (!tr_isfold) return
-        //  let flag_do_fold = false  // 防止折叠最小层
-        //  for (let j=i+1; j<l_tr.length; j++){
-        //    const tr2 = l_tr[j]
-        //    const tr_level2 = Number(tr2.getAttribute("tr_level"))
-        //    if (isNaN(tr_level2)) break
-        //    if (tr_level2<=tr_level) break
-        //    (tr_isfold == "true") ? tr2.style.display = "" : tr2.style.display = "none"
-        //    flag_do_fold = true
-        //  }
-        //  if (flag_do_fold) tr.setAttribute("is_fold", tr_isfold=="true"?"false":"true")
-        //`)
+        tr.setAttribute("onclick", `
+         const tr = this
+         const l_tr = tr.parentNode.querySelectorAll("tr")
+         const i = ${i}
+         const tr_level = Number(tr.getAttribute("tr_level"))
+         if (isNaN(tr_level)) return
+         const tr_isfold = tr.getAttribute("is_fold")
+         if (!tr_isfold) return
+         let flag_do_fold = false  // 防止折叠最小层
+         for (let j=i+1; j<l_tr.length; j++){
+           const tr2 = l_tr[j]
+           const tr_level2 = Number(tr2.getAttribute("tr_level"))
+           if (isNaN(tr_level2)) break
+           if (tr_level2<=tr_level) break
+           (tr_isfold == "true") ? tr2.style.display = "" : tr2.style.display = "none"
+           flag_do_fold = true
+         }
+         if (flag_do_fold) tr.setAttribute("is_fold", tr_isfold=="true"?"false":"true")
+        `)
         // 2. 二选一，正常绑定方法
         // 当前ob使用
-        tr.onclick = ()=>{
-          const tr_level = Number(tr.getAttribute("tr_level"))
-          if (isNaN(tr_level)) return
-          const tr_isfold = tr.getAttribute("is_fold")
-          if (!tr_isfold) return
-          let flag_do_fold = false  // 防止折叠最小层
-          for (let j=i+1; j<l_tr.length; j++){
-            const tr2 = l_tr[j]
-            const tr_level2 = Number(tr2.getAttribute("tr_level"))
-            if (isNaN(tr_level2)) break
-            if (tr_level2<=tr_level) break
-            (tr_isfold == "true") ? tr2.style.display = "" : tr2.style.display = "none"
-            flag_do_fold = true
-          }
-          if (flag_do_fold) tr.setAttribute("is_fold", tr_isfold=="true"?"false":"true")
-        }
+        // tr.onclick = ()=>{
+        //   const tr_level = Number(tr.getAttribute("tr_level"))
+        //   if (isNaN(tr_level)) return
+        //   const tr_isfold = tr.getAttribute("is_fold")
+        //   if (!tr_isfold) return
+        //   let flag_do_fold = false  // 防止折叠最小层
+        //   for (let j=i+1; j<l_tr.length; j++){
+        //     const tr2 = l_tr[j]
+        //     const tr_level2 = Number(tr2.getAttribute("tr_level"))
+        //     if (isNaN(tr_level2)) break
+        //     if (tr_level2<=tr_level) break
+        //     (tr_isfold == "true") ? tr2.style.display = "" : tr2.style.display = "none"
+        //     flag_do_fold = true
+        //   }
+        //   if (flag_do_fold) tr.setAttribute("is_fold", tr_isfold=="true"?"false":"true")
+        // }
       }
 
       // 折叠全列表格 事件绑定 // TODO，可以简化、复用。tr.onclick(这里加上可空传参)
