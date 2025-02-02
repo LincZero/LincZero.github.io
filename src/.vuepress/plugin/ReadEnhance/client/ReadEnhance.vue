5039d567cbd9e8901f292008560be6a7dbc9c245
Linc <762699299@qq.com>
2025-02-02 22:41:05 +0800

    feat(ReadEnhance): add new plugin: ReadEnhance2

--- src/.vuepress/plugin/ReadEnhance/client/ReadEnhance.vue
+++ src/.vuepress/plugin/ReadEnhance/client/ReadEnhance.vue
@@ -2,7 +2,7 @@
   <!-- 如果要调试就将第二个改成true -->
   <div class="read-enhance vp-nav-item"
     @mouseover="isShowContent = true"
-    @mouseleave="isShowContent = true"
+    @mouseleave="isShowContent = false"
   >  
     <div class="read-enhance-icon">
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open"><path d="M12 7v14"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/></svg>
