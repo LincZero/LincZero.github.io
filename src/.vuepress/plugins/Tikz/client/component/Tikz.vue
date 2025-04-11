71bdab149523c7d2be56b3a63a742b5a4d69236d
Linc <762699299@qq.com>
2025-04-11 22:58:31 +0800

    fix(tikz): tikz invalid when switch page1
    try fix, by render() and reload script tag, but fail

--- src/.vuepress/plugins/Tikz/client/component/Tikz.vue
+++ src/.vuepress/plugins/Tikz/client/component/Tikz.vue
@@ -2,10 +2,13 @@
   <div class="tikz">
     <div class="tikz-ref" ref="ref_tikz"></div>
     <!-- TODO 在未加载出来之前，给个占位和提示文字 -->
+    <!-- <button @click="readyTikzScript">准备Tikz内容</button>
+    <button @click="reloadTikZJax">重新加载Tikz</button> -->
   </div>
 </template>
 
 <script setup lang="ts">
+import { onUnmounted } from 'vue';
 import { onMounted } from 'vue';
 import { ref } from 'vue';
 
@@ -19,10 +22,16 @@ const props = defineProps<{
 
 // #region tikzjax依赖
 function loadTikZJax() {
-  if (document.querySelector('script[src="https://tikzjax.com/v1/tikzjax.js"]')) {
-    renderTikz()
-    return
+  if (document.querySelector('#tikzjax')) { // script[src="https://tikzjax.com/v1/tikzjax.js"]
+    // @ts-ignore
+    console.log('window.tikzJax2', window, window.tikzjax, window.tikzjax?.render)
+    // @ts-ignore
+    // if (window.tikzjax) window.tikzjax.render()
+    // TODO 话说重新加载也没办法自动触发自动渲染，没用
+    unloadTikZJax(); loadTikZJax(); return // 注意避免无限循环
   }
+
+  readyTikzScript()
 
   const style = document.createElement("link"); document.head.appendChild(style)
   style.rel = "stylesheet"
@@ -36,8 +45,12 @@ function loadTikZJax() {
   script.src = 'https://tikzjax.com/v1/tikzjax.js'
 
   script.onload = () => {
-    renderTikz()
     console.log('TikZJax loaded successfully')
+    // 首次加载不手动触发也行，他自己会自动触发一次
+    // @ts-ignore
+    // console.log('window.tikzJax', window, window.tikzjax, window.tikzjax?.render)
+    // @ts-ignore
+    // if (window.tikzjax) window.tikzjax.render()
   }
   script.onerror = () => {
     console.error('Failed to load TikZJax')
@@ -45,22 +58,39 @@ function loadTikZJax() {
 
   document.addEventListener('tikzjax-load-finished', onTikzFinish)
 }
+function unloadTikZJax() {
+  const els = document.querySelectorAll("#tikzjax");
+  for (const el of els) el.remove();
+
+  document.removeEventListener("tikzjax-load-finished", onTikzFinish);
+}
+async function reloadTikZJax() {
+  // TODO 话说重新加载也没办法自动触发自动渲染，没用
+  unloadTikZJax();
+  await setTimeout(() => { loadTikZJax(); }, 2000)
+  return
+}
 onMounted(() => {
   loadTikZJax()
+})
+onUnmounted(() => {
+  unloadTikZJax()
 })
 // #endregion
 
 // #region tikzjax渲染触发
 const ref_tikz = ref<HTMLElement | null>(null)
-function renderTikz() {
+function readyTikzScript() {
   if (!ref_tikz.value) return
   const script = document.createElement('script'); ref_tikz.value.appendChild(script);
 
   script.setAttribute("type", "text/tikz");
   script.setAttribute("data-show-console", "true");
-  script.textContent = renderTikz_tidy(props.data) // 可能需要tidy一下
+  script.textContent = readyTikzScript_tidy(props.data) // 可能需要tidy一下
+
+  console.log('readyTikzscript yet')
 }
-function renderTikz_tidy(tikzSource: string): string {
+function readyTikzScript_tidy(tikzSource: string): string {
   // Remove non-breaking space characters, otherwise we get errors | 删除非换行空格字符，否则我们会得到错误
   const remove = "&nbsp;";
   tikzSource = tikzSource.replaceAll(remove, "");
@@ -81,7 +111,12 @@ function renderTikz_tidy(tikzSource: string): string {
 // #region tikzjax回调、后处理
 function onTikzFinish (e) {
   console.log('tikz finish-------------------------------\n', e)
-  // 这里可以仿照 obsidian tikz 的做法做反色，懒得，直接填白色背景算了
+  // TODO fix 目前无效，因为ob-tikzjax的包是有 finish 事件的，
+  // 而网络加载的 https://tikzjax.com/v1/tikzjax.js 似乎没有这个东西
+  // 怀疑是不是v0和v1的区别
+
+  // TODO 这里可以做显示出来之前的占位
+  // TODO 这里可以仿照 obsidian tikz 的做法做反色。目前懒得，直接填白色背景算了
 }
 // #endregion
 </script>
