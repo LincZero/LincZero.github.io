9b7cf20520ae7378529d80279426a2950dd1a01d
LincZero <762699299@qq.com>
2025-05-08 20:42:17 +0000

    Deploying to gh-pages from @ LincZero/LincZero.github.io@43fc65f5f18d179a856fb1b9303611979295f9f3 🚀

---
+++ assets/CppProperties.json.html-CgbwNzcT.js
@@ -0,0 +1,22 @@
+import{_ as e,e as a,h as t,r as s,o}from"./app-jh-UnpcC.js";const i={};function r(m,p){const n=s("VueFlow");return o(),a("div",null,[t(n,{type:"nodeflow-comfyui",data:`{
+  "configurations": [
+    {
+      "inheritEnvironments": [
+        "msvc_x86"
+      ],
+      "name": "x86-Debug",
+      "includePath": [
+        "\${env.INCLUDE}",
+        "\${workspaceRoot}\\\\**"
+      ],
+      "defines": [
+        "WIN32",
+        "_DEBUG",
+        "UNICODE",
+        "_UNICODE"
+      ],
+      "intelliSenseMode": "windows-msvc-x86"
+    }
+  ]
+}
+`})])}const c=e(i,[["render",r],["__file","CppProperties.json.html.vue"]]),h=JSON.parse('{"path":"/MdNote_Public/01.%20DesignAndDevelop/Develop/01.%20Language/C__/10.%20%E7%9B%B8%E5%85%B3%E4%BB%A3%E7%A0%81/CppProperties.json.html","title":"CppProperties.json","lang":"zh-CN","frontmatter":{"description":"{ \\"configurations\\": [ { \\"inheritEnvironments\\": [ \\"msvc_x86\\" ], \\"name\\": \\"x86-Debug\\", \\"includePath\\": [ \\"env.INCLUDE\\",\\"{workspaceRoot}\\\\**\\" ], \\"defines\\": [ \\"WIN32\\", \\"_DEBUG\\", \\"UNICO...","head":[["meta",{"property":"og:url","content":"https://LincZero.github.io/MdNote_Public/01.%20DesignAndDevelop/Develop/01.%20Language/C__/10.%20%E7%9B%B8%E5%85%B3%E4%BB%A3%E7%A0%81/CppProperties.json.html"}],["meta",{"property":"og:site_name","content":"Linc 的小站"}],["meta",{"property":"og:description","content":"{ \\"configurations\\": [ { \\"inheritEnvironments\\": [ \\"msvc_x86\\" ], \\"name\\": \\"x86-Debug\\", \\"includePath\\": [ \\"env.INCLUDE\\",\\"{workspaceRoot}\\\\**\\" ], \\"defines\\": [ \\"WIN32\\", \\"_DEBUG\\", \\"UNICO..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":null,\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"LincZero\\",\\"url\\":\\"https://github.com/LincZero/\\"}]}"]],"layout":"Layout"},"git":{},"readingTime":{"minutes":0.06,"words":18},"filePathRelative":"MdNote_Public/01. DesignAndDevelop/Develop/01. Language/C++/10. 相关代码/CppProperties.json","excerpt":"<p>{<br>\\n\\"configurations\\": [<br>\\n{<br>\\n\\"inheritEnvironments\\": [<br>\\n\\"msvc_x86\\"<br>\\n],<br>\\n\\"name\\": \\"x86-Debug\\",<br>\\n\\"includePath\\": [<br>\\n\\"<span v-pre=\\"\\" class=\\"katex\\"><span class=\\"katex-mathml\\"><math xmlns=\\"http://www.w3.org/1998/Math/MathML\\"><semantics><mrow><mrow><mi>e</mi><mi>n</mi><mi>v</mi><mi mathvariant=\\"normal\\">.</mi><mi>I</mi><mi>N</mi><mi>C</mi><mi>L</mi><mi>U</mi><mi>D</mi><mi>E</mi></mrow><mi mathvariant=\\"normal\\">\\"</mi><mo separator=\\"true\\">,</mo><mi mathvariant=\\"normal\\">\\"</mi></mrow><annotation encoding=\\"application/x-tex\\">{env.INCLUDE}\\",\\n        \\"</annotation></semantics></math></span><span class=\\"katex-html\\" aria-hidden=\\"true\\"><span class=\\"base\\"><span class=\\"strut\\" style=\\"height:0.8889em;vertical-align:-0.1944em;\\"></span><span class=\\"mord\\"><span class=\\"mord mathnormal\\">e</span><span class=\\"mord mathnormal\\">n</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.03588em;\\">v</span><span class=\\"mord\\">.</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.07847em;\\">I</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.07153em;\\">NC</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.10903em;\\">LU</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.02778em;\\">D</span><span class=\\"mord mathnormal\\" style=\\"margin-right:0.05764em;\\">E</span></span><span class=\\"mord\\">\\"</span><span class=\\"mpunct\\">,</span><span class=\\"mspace\\" style=\\"margin-right:0.1667em;\\"></span><span class=\\"mord\\">\\"</span></span></span></span>{workspaceRoot}\\\\**\\"<br>\\n],<br>\\n\\"defines\\": [<br>\\n\\"WIN32\\",<br>\\n\\"_DEBUG\\",<br>\\n\\"UNICODE\\",<br>\\n\\"_UNICODE\\"<br>\\n],<br>\\n\\"intelliSenseMode\\": \\"windows-msvc-x86\\"<br>\\n}<br>\\n]<br>\\n}</p>","autoDesc":true,"bioChainData":{"outlink":[],"backlink":[],"localMap":{"nodes":[{"id":"MdNote_Public/01. DesignAndDevelop/Develop/01. Language/C++/10. 相关代码/CppProperties.json","value":{"title":"CppProperties.json","path":"MdNote_Public/01. DesignAndDevelop/Develop/01. Language/C++/10. 相关代码/CppProperties.json","outlink":[],"backlink":[]}}],"links":[]}}}');export{c as comp,h as data};
---
+++ assets/stateDiagram-v2-HMOQ2HEA-DbvD_xF1.js
@@ -0,0 +1,1 @@
+import{a as s,B as t,r as a,I as e}from"./chunk-LDJ5YOBB-BJ-puvzF.js";import"./chunk-P7KHIEPL-CkT85Q-g.js";import{m as o}from"./mermaid.esm.min-BtJTlDNJ.js";import"./app-jh-UnpcC.js";var l={parser:e,db:a,renderer:t,styles:s,init:o(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute,a.clear()},"init")};export{l as diagram};
