d3f0f1da473c007597783b9fdd86148425b1e722
Linc <762699299@qq.com>
2026-03-28 15:07:10 +0800

    update: markdown-it-any-block@beta3
    核心: 避免在 client 环境引入 jsdom 依赖，并避免由此引起的大量警告打印

--- src/.vuepress/plugins/AnyBlock/node/index.ts
+++ src/.vuepress/plugins/AnyBlock/node/index.ts
@@ -1,10 +1,16 @@
-import { getDirname, path } from "@vuepress/utils"
+import { path } from "@vuepress/utils"
 
-import { ab_mdit, ab_mdit_client, jsdom_init } from "markdown-it-any-block" // npm
+import { ab_mdit, ab_mdit_client } from "markdown-it-any-block"
+
 const CLIENT_MODE = false
-if (!CLIENT_MODE) jsdom_init(true, true) // 如果模块根部有直接使用的，可能要改成 await jsdom_init() 同步操作
+if (!CLIENT_MODE && typeof document === 'undefined') {
+  // (可选) 如果你要在 node 环境渲染而非 client 中渲染，则加上下面的语句。在 client 端不要这样做
+  import("markdown-it-any-block/node").then(({ jsdom_init }) => {
+    jsdom_init(true, true)  // 如果模块根部有直接使用的，可能要改成 await jsdom_init() 同步操作
+  })
+}
 
-export default (options, ctx) => {
+export default (_options, _ctx) => {
   return {
     name: 'vuepress-plugin-any-block',
     clientConfigFile: path.resolve(__dirname, '../client/clientConfig.ts'),
